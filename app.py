f69328b6ab5b1920ffbc64e80846322694a8b3c5
Pavel Miroanicenco <pavel@Ghennadiis-MacBook-Pro-2.local>
2020-06-19 12:37:59 +0100

    updating comments fro relevancy

--- app.py
+++ app.py
@@ -16,23 +16,20 @@ app.config["DEBUG"] = True
 
 @app.route('/', methods=['POST'])
 def api_text():
-    # Check if an ID was provided as part of the URL.
-    # If ID is provided, assign it to a variable.
-    # If no ID is provided, display an error in the browser.
+    #receives the json form POST request
     content = request.json
     text = content['image']
-    # Create an empty list for our results
-    results = "helo"
+    # Create an empty var for our results
+    results = ""
 
-    # Loop through the data and match results that fit the requested ID.
-    # IDs are unique, but other fields might return many results
+    #receives image and saves it on the server as png
     im = Image.open(BytesIO(base64.b64decode(text)))
     im.save('image.png', 'PNG')
-
+    #using pytesseract and train data to read the message off screen
     results = pytesseract.image_to_string(Image.open("image.png"))  # We'll use Pillow's Image class to open the image and pytesseract to detect the string in the image
     print(results)
-    # Use the jsonify function from Flask to convert our list of
-    # Python dictionaries to the JSON format.
+    # Use the jsonify function from Flask to convert our string
+    #  to the JSON format.
     return jsonify(image=results)
 
 if __name__ == '__main__':
